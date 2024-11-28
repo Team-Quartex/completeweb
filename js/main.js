9fb1b9e151136a645fc5a30d0f56b0c1c24a45e4
Shana2002 <hansakaravi02@gmail.com>
2024-11-28 20:14:33 +0530

    logout function

--- js/main.js
+++ js/main.js
@@ -13,3 +13,22 @@ document.addEventListener('DOMContentLoaded',()=>{
     .catch(error => window.location.href = 'login.html');
     
 })
+
+document.getElementById('logout-btn').addEventListener('click',()=>{
+    fetch('http://localhost:8000/api/users/logout', {
+        method: 'GET',
+        credentials: 'include'  // Sends cookies with the request
+    })
+    .then(response => {
+        location.reload();
+        window.location.href = 'login.html'})
+    .then(data => {
+        if (data.cookieFound) {
+            window.location.href = 'login.html'
+            location.reload();
+        } else {
+            window.location.href = 'login.html' ;
+        }
+    })
+    .catch(error => window.location.href = 'login.html');
+})
